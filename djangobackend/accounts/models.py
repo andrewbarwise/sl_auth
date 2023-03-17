7dd69132a7ca973de58633528d8944de0a353cff
Andrew Barwise <andrew.barwise@gmail.com>
2023-03-17 14:10:12 +0000

    defined User model

--- djangobackend/accounts/models.py
+++ djangobackend/accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.dispatch import receiver
+from django.urls import reverse
 
 # Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(max_length=254, unique=True)
+    username = models.CharField(max_length=150, unique=True)
+
+class Meta:
+    db_table = 'User'
