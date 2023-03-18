0180aaac1e148a6975896539d63fe8e07d86b1ad
Andrew Barwise <andrew.barwise@gmail.com>
2023-03-18 15:50:59 +0000

    update

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,7 +1,36 @@
 import streamlit as st
+import requests
 
-def login_page():
-    pass
+def login_page(applicant_token):
+    if (applicant_token):
+        st.write("The user is logged in.")
+
+        st.write('login content and logic comes here')
+
+    else:
+        with st.form("my_form"):
+            email = st.text_input(label='email')
+            password = st.text_input(label='password', type='password')
+            submit_res = st.form_submit_button(label='Login')
+
+            if submit_res:
+                st.write('Login clicked.')
+
+                headers = {'content-type':'application/json; charset=utf-8'}
+                response = requests.post('http://127.0.0.1:8000/api/accounts/api_auth/',
+                    headers=headers, json={'email':email, 'password':password})
+                
+                response_json = response.json()
+
+                if response.status_code == 200:
+                    applicant_token = response_json['token']
+
+                    if applicant_token:
+                        st.session_state.key = 'applicant-token'
+                        st.session_state['applicant-token'] = applicant_token
+                        st.experiment_rerun()
+
+    
 
 def register():
     pass
